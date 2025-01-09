a978823ac963c190a6a55108ab72ee39fb31d071
Dagim <dagimgetaW27@gmail.com>
2025-01-09 20:36:05 +0300

    popup functionality

---
+++ TODOLIST(CSEC Bootcamp)/script.js
@@ -0,0 +1,9 @@
+const handlePopUp = () => {
+  document.getElementById("popup").style.display = "block";
+  document.querySelector(".black_bg").style.display = "block";
+};
+
+const handleClosePopUp = () => {
+  document.getElementById("popup").style.display = "none";
+  document.querySelector(".black_bg").style.display = "none";
+};
