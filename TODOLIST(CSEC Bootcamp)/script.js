39d5df2456059f3c962c920163cadadc05f04557
Dagim <dagimgetaW27@gmail.com>
2025-01-10 11:01:29 +0300

    color theme toggle func added

--- TODOLIST(CSEC Bootcamp)/script.js
+++ TODOLIST(CSEC Bootcamp)/script.js
@@ -7,3 +7,28 @@ const handleClosePopUp = () => {
   document.getElementById("popup").style.display = "none";
   document.querySelector(".black_bg").style.display = "none";
 };
+
+const handleColor = () => {
+  const colorDiv = document.querySelector(".color");
+  const moonIcon = colorDiv.children[0];
+  const sunIcon = colorDiv.children[1];
+
+  if (moonIcon.style.display !== "none") {
+    moonIcon.style.display = "none";
+    sunIcon.style.display = "inline-block";
+    document.body.classList.add("darkmode");
+  } else {
+    moonIcon.style.display = "inline-block";
+    sunIcon.style.display = "none";
+    document.body.classList.remove("darkmode");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const colorDiv = document.querySelector(".color");
+  const moonIcon = colorDiv.children[0];
+  const sunIcon = colorDiv.children[1];
+
+  moonIcon.style.display = "inline-block";
+  sunIcon.style.display = "none";
+});
